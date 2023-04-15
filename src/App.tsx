fc94864645a0fd98827d00d285b898d70cc01025
KirillKozlo <116728298+KIK9292@users.noreply.github.com>
2023-04-16 00:24:55 +0300

    samuray-9 typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Header";
+import {Technologies} from "./Technologies";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai!!!!! Let's go!
-    </div>
+  return (<div>
+            <Header/>
+            <Technologies/>
+          </div>
   );
 }
 
